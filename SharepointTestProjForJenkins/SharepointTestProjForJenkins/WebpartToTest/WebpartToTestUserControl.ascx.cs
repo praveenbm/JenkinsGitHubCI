17d1b1805941e1df86fff1629ab672cb67bc88ec
unknown <Praveen.bisaravalli@emc.com>
2014-07-07 11:53:18 +0530

    checkin

--- SharepointTestProjForJenkins/SharepointTestProjForJenkins/WebpartToTest/WebpartToTestUserControl.ascx.cs
+++ SharepointTestProjForJenkins/SharepointTestProjForJenkins/WebpartToTest/WebpartToTestUserControl.ascx.cs
@@ -15,6 +15,7 @@ namespace SharepointTestProjForJenkins.WebpartToTest
         protected void Button1_Click(object sender, EventArgs e)
         {
             Label1.Text = "You clicked me!!";
+            Label1.Font.Italic= true;
         }
     }
 }
