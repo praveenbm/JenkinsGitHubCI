cc67af24ed501d0caaf56c4cae1b3b4fae1d84f5
unknown <Praveen.bisaravalli@emc.com>
2014-07-16 12:18:36 +0530

    test

--- SharepointTestProjForJenkins/SharepointTestProjForJenkins.Test/Class1.cs
+++ SharepointTestProjForJenkins/SharepointTestProjForJenkins.Test/Class1.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using SharepointTestProjForJenkins;
 using NUnit.Framework;
 
 namespace SharepointTestProjForJenkins.Test
